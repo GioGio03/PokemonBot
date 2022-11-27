38f07fb1bd9b8f08397a4f56e9883ca952210025
Josuke <84544068+GioGio03@users.noreply.github.com>
2022-11-27 10:52:19 +0100

    Update guild.js

--- models/guild.js
+++ models/guild.js
@@ -4,7 +4,7 @@ const GuildSchema = new mongoose.Schema({
     id: { type: String, required: true },
     spawnchannel: { type: String, default: null },
     disabledChannels: { type: Array, default: [] },
-    spawnbtn: { type: Boolean, default: true },
+    spawnbtn: { type: Boolean, default: false },
     levelupchannel: { type: String, default: null },
     levelupbtn: { type: Boolean, default: true },
     blacklist: { type: Boolean, default: false },
